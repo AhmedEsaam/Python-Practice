5a36c808ae818d3474be95a81640868af3bf3877
Ahmed Essam <ahmed.e.mogy@gmail.com>
2024-04-26 11:20:28 +0300

    Adding The Python Lab

---
+++ Python_Lab.py
@@ -0,0 +1,30 @@
+if True:
+    print('Answer \
+the phone!')
+    print('True')
+else:
+    print('False')
+
+word = 'word'
+sentence = "This is a sentence."
+paragraph = '''  This is a paragraph. It's
+made up of multible lines'''
+paragraph2 = """  Same as
+this paragraph."""
+print(word)
+print(sentence)
+print(paragraph)
+print(paragraph2)
+
+# Dynamic Typing
+counter = 100               # An integer assignmnet
+miles   = 1000.0            # A floating point
+name    = 'John'            # A string
+print(name)
+
+name = 5.6
+print(name)
+
+# Multiple Assignment 
+a = b = c = 1               # assign a single value to several variables simultaneously
+a, b, c = 1, 2, "John"      # assign multiple objects to multiple variables  
